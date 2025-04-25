4019a3d8df25f3add9606850f7a2b40d1135338f
Eduardo Armando Kubanza <edukubanza17@gmail.com>
2025-04-25 10:43:52 +0100

    update

--- src/helpers/buildUrl.ts
+++ src/helpers/buildUrl.ts
@@ -1,5 +1,5 @@
 export async function buildUrl(...path: string[]) {
-  const apiURL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:7892";
+  const apiURL = "http://localhost:7892";
 
   return [apiURL, ...path].join("/");
 }
